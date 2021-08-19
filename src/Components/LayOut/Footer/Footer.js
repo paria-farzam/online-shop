85250518f5fca0d09b79308180594d8966530cbc
Paria Farzam <pariafarzam79@gmail.com>
2021-08-19 14:45:12 +0430

    footer styles completed

--- src/Components/LayOut/Footer/Footer.js
+++ src/Components/LayOut/Footer/Footer.js
@@ -1,28 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./mobile.css";
 
 const Footer = () => {
-    return (
-        <div id='footer'>
-            <div>
-                <h2>شماره تماس</h2>
-                <h3>09123456789</h3>
-                <h3>051-39999999</h3>
-            </div>
-            <div>
-                <h2>ایمیل</h2>
-                <h3>example@gmail.com</h3>
-            </div>
-            <div>
-                <h2>شبکه های اجتماعی</h2>
-                <Link to='#'><i className="fab fa-instagram"></i></Link>
-                <Link to='#'><i className="fab fa-telegram-plane"></i></Link>
-                <Link to='#'><i className="fab fa-facebook-f"></i></Link>
-            </div>
-            <Link to='/contact'>ارتباط با ما</Link>
-            <Link to='/about'>درباره ما</Link>
+  return (
+    <>
+      {/* <hr className="text-muted m-0" /> */}
+      <div id="footer" className="row mx-0 py-4 px-4 text-dark">
+        <Link
+          className="col-6 text-decoration-none about-contact fs-1"
+          to="/contact"
+        >
+            <span className='fs-1'>&#10022;</span> 
+          ارتباط با ما
+        </Link>
+
+        <Link
+          className="col-6 text-decoration-none about-contact fs-1"
+          to="/about"
+        >
+            <span className='fs-1'>&#10022;</span> 
+          درباره ما
+        </Link>
+        <div className="text-muted col-5 fs-3 my-4">
+          <h2>شماره تماس</h2>
+          <h3 className="fs-5">09153456789</h3>
+          <h3 className="fs-5">051-39999999</h3>
         </div>
-    );
+
+        <div className="text-muted col-7 fs-3 my-4">
+          <h2><span className='fs-1'>&#9993;</span>  ایمیل</h2>
+          <h3 className="fs-5">example@gmail.com</h3>
+        </div>
+
+        <div className="row social-media-container m-0 py-3 align-content-center justify-content-between">
+          <h2 className="col-6 mb-0 align-self-center">ما را دنبال کنید:</h2>
+          <div className="row icon-container col-6 justify-content-center align-content-center">
+            {/* <Link
+              className="col-4 social-media-icon text-decoration-none text-dark"
+              to="#"
+            >
+              <img
+                className="icon"
+                src="https://img.icons8.com/ios/50/000000/linkedin-2--v1.png"
+              />
+            </Link> */}
+            <Link
+              className="col-4 social-media-icon text-decoration-none text-dark mx-1+"
+              to="#"
+            >
+              <img
+                className="icon"
+                src="https://img.icons8.com/fluency-systems-regular/32/000000/telegram-app.png"
+              />
+            </Link>
+            <Link
+              className="col-4 social-media-icon text-decoration-none text-dark mx-1+"
+              to="#"
+            >
+              <img
+                className="icon"
+                src="https://img.icons8.com/ios/50/000000/instagram-new--v1.png"
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
